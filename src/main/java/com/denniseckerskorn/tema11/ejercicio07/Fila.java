c16be6604b10fd0b4d9f156874f7c79291255834
Dennis <denniseckerskorn@gmail.com>
2024-06-07 17:05:07 +0200

    Update

--- src/main/java/com/denniseckerskorn/tema11/ejercicio07/Fila.java
+++ src/main/java/com/denniseckerskorn/tema11/ejercicio07/Fila.java
@@ -3,8 +3,9 @@ package com.denniseckerskorn.tema11.ejercicio07;
 import java.util.List;
 
 public class Fila {
+    private static final int DEFAULT_ASIENTOS = 10;
     private final List<Asiento> asientos;
     private final int numFila;
-    private final
+
 
 }
